3e19604ad23b54c5aaaf967d168d7b55f83c0223
iawaisaleem <awaisalee000@gmail.com>
2026-07-19 18:08:10 +0500

    checking2

--- config.py
+++ config.py
@@ -3,22 +3,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print("MYSQLHOST =", os.getenv("MYSQLHOST"))
+print("MYSQLPORT =", os.getenv("MYSQLPORT"))
+print("MYSQLUSER =", os.getenv("MYSQLUSER"))
+print("MYSQLPASSWORD =", os.getenv("MYSQLPASSWORD"))
+print("MYSQLDATABASE =", os.getenv("MYSQLDATABASE"))
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    # MySQL
     SQLALCHEMY_DATABASE_URI = (
-    f"mysql+pymysql://"
-    f"{os.getenv('MYSQL_USER')}:"
-    f"{os.getenv('MYSQL_PASSWORD')}@"
-    f"{os.getenv('MYSQL_HOST')}:"
-    f"{os.getenv('MYSQL_PORT',3306)}/"
-    f"{os.getenv('MYSQL_DB')}"
-)
+        f"mysql+pymysql://"
+        f"{os.getenv('MYSQLUSER')}:"
+        f"{os.getenv('MYSQLPASSWORD')}@"
+        f"{os.getenv('MYSQLHOST')}:"
+        f"{os.getenv('MYSQLPORT')}/"
+        f"{os.getenv('MYSQLDATABASE')}"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Mail
-    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
     MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
     MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "False") == "True"
